1e4008ed04eb71319614cd86b61ee966fdc85338
pmatyus <>
2021-05-12 19:22:55 +0000

    exercise solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    age = int(input("How old are you?"))
+    if (age < 0 or age > 120):
+      print("Impossible!")
+    else:
+      print("OK")
 
 if __name__ == '__main__':
     main()
